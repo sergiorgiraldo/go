88eaa421e8698b321a472fa99a9c1328817bb5da
Sergio Giraldo <sergio@giraldo.com.br>
2023-10-11 22:48:52 +0200

    feat: tests with versioning from pre-commit 001 ::by sergio giraldo @ 20231011T2248CEST, gpg signed

---
+++ v.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+version_=$(cat ./VERSION)
+
+if [ "$1" == "major" ]; then
+	major_=$(semver -t <<< "$version_" | awk -F '\t' '{ print ++$1 "." 0 "." 0 }')
+	echo $major_ > VERSION
+elif [ "$1" == "minor" ]; then
+	minor_=$(semver -t <<< "$version_" | awk -F '\t' '{ print $1 "." ++$2 "." 0 }')
+	echo $minor_ > VERSION
+elif [ "$1" == "patch" ]; then
+	patch_=$(semver -t <<< "$version_" | awk -F '\t' '{ print $1 "." $2 "." ++$3 }')
+	echo $patch_ > VERSION
+else
+  echo "Invalid parameter passed"
+  exit 1
+fi
+
+
+
+
+
+
+
